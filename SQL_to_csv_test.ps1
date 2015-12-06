7c034709772f2d68e1acae3d778d17929b85e7d7
Aldous Birchall <aldous_birchall@hotmail.com>
2015-12-06 12:25:22 +0000

    Added test scripts

---
+++ SQL_to_csv_test.ps1
@@ -0,0 +1,8 @@
+﻿##test script to extract data from SQL DB and dump in csv
+## Uses sqlps cmdlet. Note that I have to explicitly point to Database and server... 
+##...instance. Perhaps set globally?
+## '-DisableNameChecking' and '-NoType' give clean output
+Import-module "sqlps" -DisableNameChecking
+Invoke-Sqlcmd -Query "SELECT * FROM cds_yieldCurves;" `
+–Database “Strata_TestDB” -ServerInstance "MINIDESKTOP\SQLEXPRESS" `
+| Export-Csv -NoType C:\Users\Aldous\Desktop\Test_SQL_export.csv
